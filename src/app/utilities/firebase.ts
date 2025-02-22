8cdc85f3583e524c14494088b4b109abce671812
Muhammed Bilal K <84187915+Muhammed-Bilal-K@users.noreply.github.com>
2025-02-22 10:14:33 +0530

    Update firebase.ts

--- src/app/utilities/firebase.ts
+++ src/app/utilities/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth , GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDevBeidTT6zemFIA6S_FJV5l2f6-o0FA4",
-  authDomain: "jobpilot-17ae5.firebaseapp.com",
-  projectId: "jobpilot-17ae5",
-  storageBucket: "jobpilot-17ae5.appspot.com",
-  messagingSenderId: "713141779725",
-  appId: "1:713141779725:web:29231f96947c52e2128444",
-  measurementId: "G-6NG8MMFRFB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
